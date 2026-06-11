bb7281c9778d3dfc8245694be0063f96080ab8b5
vladislav.golodok <vladislav.golodok@axenix.pro>
2026-06-11 15:21:46 +0300

    Task-3: отступы

--- budgetboard/app/layouts/default.vue
+++ budgetboard/app/layouts/default.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex min-h-screen">
-    <AppSidebar/>
-<!--    <div v-if="isOpen">-->
-<!--      <AppSidebar/>-->
-<!--    </div>-->
+    <AppSidebar />
+    <!--    <div v-if="isOpen">-->
+    <!--      <AppSidebar/>-->
+    <!--    </div>-->
     <main class="min-w-0 flex-1 p-6">
-<!--      <AppBurgerButton class="ml-7 m-5.5 mr-0" :isOpen="isOpen" @click="isOpen = !isOpen"/>-->
-      <slot/>
+      <!--      <AppBurgerButton class="ml-7 m-5.5 mr-0" :isOpen="isOpen" @click="isOpen = !isOpen"/>-->
+      <slot />
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
-//TODO вернуть бургер
-// import AppBurgerButton from '../components/app/AppBurgerButton.vue';
-import AppSidebar from '../components/app/AppSidebar.vue';
+  //TODO вернуть бургер
+  // import AppBurgerButton from '../components/app/AppBurgerButton.vue';
+  import AppSidebar from "../components/app/AppSidebar.vue";
 
-// const isOpen = ref(false)
+  // const isOpen = ref(false)
 </script>
